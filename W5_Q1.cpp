1f27e23fac2366ac57062239ff82c6a2128d236d
M1chael910 <mpmurphybusiness@gmail.com>
2023-02-19 15:09:27 -0600

    Finished Q1

--- W5_Q1.cpp
+++ W5_Q1.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
+using namespace std;
+
+void displayNumbersFromRange();
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int x = 0;
+    for (x; x <= 3; x++) {
+        displayNumbersFromRange();
+    }
     return 0;
 }
+
+void displayNumbersFromRange() {
+    int start = 0;
+    int end = 0;
+    cout << "\nEnter a starting and ending number.";
+    cin >> start >> end;
+    cout << "printing numbers from " << start << " to " << end << ":\n\n";
+    for (start; start <= end; start++) {
+        //Uses the start variable as a counter
+        cout << start << endl;
+    }
+}
+
